2e1944959ff6a3a487b39bcd854c8e4a4e5d071f
Sehbazz Virk <virk.sehbazz12@gmail.com>
2022-12-07 17:25:49 -0700

    clientConn.hpp structure complete, notes about function definitions included

--- server/clientConn.hpp
+++ server/clientConn.hpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <string>
 #include <deque>
+#include <mutex>
 
 #define BUFFER_SIZE 1024
 
@@ -13,10 +14,10 @@
 class clientConn {
 
     private:
-        std::array<char, BUFFER_SIZE> recv;
-        std::array<char, BUFFER_SIZE> send;
+        std::array<char, BUFFER_SIZE> buffer;
 
-        std::deque<std::string> messageQueue();
+        std::deque<std::string> sendQueue();
+        std::deque<std::string> recvQueue();
 
         int fd;
 
@@ -25,9 +26,26 @@ class clientConn {
         clientConn(int socketFD);
         ~clientConn();
 
-        void readMessages(); // function to listen to communication socket, passed to thread 
-        void sendMessage(); // function to send message on top of queue to client, passed to thread
+        /**
+         * Control Flow:
+         * While True:
+         * Lock access to sendQueue (blocks until access acquired)
+         * send whole sendqueue to client
+         * unlock sendQueue
+         * 
+         * Lock access to recvQueue (blocks until access acquired)
+         * while read socket not empty
+         * non-blocking read of file descriptor
+         ** If no messages recieved, read socket empty
+         ** else add message to recvQueue for server processing
+         * after broken out of while: unlock recvQueue
+        */
+        void processMessages(); // function passed to thread for reading and writing messages
 
+        /**
+         * Functions below NEED to mutex access to recvQueue and sendQueue. They are functions for the server to get and send messages to this client
+         * These will run in the main thread while the processMessages() function will have its own.
+        */
         std::string getNextMessage(); // send message on top of recv queue to server
         std::string queueSendMessage(); // let server queue another message to send
 
